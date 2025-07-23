813e2548f5c6f6341f28f9bb89b9bfc27bdd4c8c
KunderAishwarya <53344332+KunderAishwarya@users.noreply.github.com>
2025-07-23 21:46:13 +0530

    Time: 1 ms (72.17%) | Memory: 45.4 MB (93.46%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,23 +1,48 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> row = new HashSet<>();
-        Set<Integer> col = new HashSet<>();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == 0) {
-                    row.add(i);
-                    col.add(j);
+        boolean firstrow = false;
+        boolean firstcol = false;
+        for(int i =0; i < matrix.length; i++){
+            if(matrix[i][0]  == 0){
+                firstcol = true;
+            }
+        }
+
+        for(int j =0; j < matrix[0].length; j++){
+            if(matrix[0][j]  == 0){
+                firstrow = true;
+            }
+        }
+
+        for(int i =1; i < matrix.length; i ++){
+            for(int j = 1; j < matrix[i].length; j ++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for (int r : row) {
-            for (int k = 0; k < matrix[r].length; k++) {
-                matrix[r][k] = 0;
+
+        
+
+        for(int i=1; i < matrix.length; i++){
+            for(int j =1; j < matrix[0].length; j++){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }                
             }
         }
-        for(int c : col){
-            for(int l=0; l < matrix.length; l++){
-                matrix[l][c] = 0;
+
+        if(firstrow){
+            for(int i =0; i < matrix[0].length; i++){
+                matrix[0][i]=0;
+            }
+        }
+        
+        
+        if(firstcol){
+            for(int j = 0; j < matrix.length; j++){
+                matrix[j][0]=0;
             }
         }
     }
